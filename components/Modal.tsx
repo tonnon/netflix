80e8601ee8fbc73a891832b368d0a6ee9e649ff8
tonnon <lucastonnon@gmail.com>
2022-08-29 21:37:01 -0300

    fetch movie trailer

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,36 @@
 import { XIcon } from '@heroicons/react/solid'
 import MuiModal from '@mui/material/Modal'
+import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
+import { modalState, movieState } from '../atoms/modalAtom'
+import {Element, Genre} from '../typings'
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
-
+  const [movie, setMovie] = useRecoilState(movieState)
+  const [trailer, setTrailer] = useState('')
+  const [genres, setGenres] = useState<Genre[]>()
+  
+  useEffect(() => {
+    if (!movie) return
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+        movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+        process.env.NEXT_PUBLIC_API_KEY
+      }&language=en-US&append_to_response=videos`).then((response) => response.json())
+      if (data?.videos) {
+        const index = data.videos.results.findIndex((element: Element) => element.type === 'Trailer')
+        setTrailer(data.videos?.results[index]?.key)
+      }
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
+    }
+    fetchMovie()
+  }, [movie])
+  
   const handleClose = () => {
     setShowModal(false)
   }
@@ -16,6 +41,9 @@ function Modal() {
         <button onClick={handleClose} className="modalButton absolute right-5 top-6 !z-40 h-9 w-9 border-none bg-[#181818]">
           <XIcon className='h-6 w-6'/>
         </button>
+        <div>
+          
+        </div>
       </>
     </MuiModal>
   )
